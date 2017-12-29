083c97c96d98dee4efcbe743e9aa0c17c6d877c0
Roy Lu <royvbtw@gmail.com>
2017-12-30 03:14:56 +0800

    Add a click-to-copy feature in the post info-box
    + Add a click-to-copy feature in the post info-box
    
    Once a user clicks the post-id in the info-box, the post-id will
    automatically copy to the clipboard.
    
    This feature is useful when a user wants the post-id to download
    the post images by **Beautyland-image-downloader**.
    
    + Add react-bootstrap dependency
    + Modify the css style for /about

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,7 @@
  */
 
 import React, { Component } from 'react';
+
 import './About.css';
 
 
@@ -13,10 +14,10 @@ export default class About extends Component {
       <div className='about'>
         <p>This project is for demo purpose.</p>
         <p>Project started from Aug 2017 by Roy Lu(royvbtw)</p>
-        <p>You can find project codes in <a href='https://github.com/royvbtw/beautyland'>github</a></p>
+        <p>You can find the project codes in <a href='https://github.com/royvbtw/beautyland'>github</a></p>
         <p>And this is my place <a href='https://royvbtw.uk'>https://royvbtw.uk</a></p>
 
-        <p>T0225</p>
+        <p>T2040</p>
       </div>
     );
   }
--- src/components/LightBox.js
+++ src/components/LightBox.js
@@ -5,18 +5,31 @@
 
 
 import React, { Component } from 'react';
+import { Tooltip, OverlayTrigger } from 'react-bootstrap';
+
 import './LightBox.css';
 
 export default class LightBox extends Component{
   closeLightBox = () => {
     this.props.onCloseLightBox();
+    this.postIdText = null;
   };
 
   onInfoBox = (e) => {
     e.stopPropagation();
   };
+
+  onClickToCopy = (e) => {
+    console.log(`click to copy`);
+    this.postIdText.select();
+    document.execCommand('copy');
+  };
   
   render(){
+    const tooltip = (
+      <Tooltip id="tooltip">Click to copy</Tooltip>
+    );
+
     let content = null;
     if(this.props.type === 'image'){
       content = <img className='imageBoxContent' src={this.props.content} alt=''/>;
@@ -30,6 +43,16 @@ export default class LightBox extends Component{
         <ul>
           <li>{this.props.content.title}</li>
           <li>Author: {this.props.content.author}</li>
+          <li>
+          Post ID<span style={{color: 'grey'}}>(Click to copy)</span>: 
+            <OverlayTrigger placement="top" overlay={tooltip}>
+              <input type='text' id='post-id' value={this.props.content.postId}
+                onClick={this.onClickToCopy}
+                ref={ (node) => {this.postIdText = node;} }
+                readOnly
+              />
+            </OverlayTrigger>
+          </li>
           <li>
             <a href={this.props.content.link} rel="noopener noreferrer" target='_blank'>
               {this.props.content.link}
